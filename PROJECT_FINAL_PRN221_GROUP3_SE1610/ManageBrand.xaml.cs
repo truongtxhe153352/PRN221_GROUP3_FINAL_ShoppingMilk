25d69d99806e15df1f2a80835c37ccdccd262c20
Cuongnvhs150257 <97275308+Cuongnvhs150257@users.noreply.github.com>
2023-03-24 19:08:31 +0700

    add manage brand and category

---
+++ PROJECT_FINAL_PRN221_GROUP3_SE1610/ManageBrand.xaml.cs
@@ -0,0 +1,55 @@
+﻿using PROJECT_FINAL_PRN221_GROUP3_SE1610.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PROJECT_FINAL_PRN221_GROUP3_SE1610
+{
+    /// <summary>
+    /// Interaction logic for ManageBrand.xaml
+    /// </summary>
+    public partial class ManageBrand : Window
+    {
+        shoppingMilkPrn221Context context = new shoppingMilkPrn221Context();
+        public ManageBrand()
+        {
+            InitializeComponent();
+            loadData();
+        }
+
+        private void loadData()
+        {
+            lvBrand.ItemsSource = context.Brands.ToList();
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PROJECT_FINAL_PRN221_GROUP3_SE1610/ManageCategory.xaml.cs
@@ -0,0 +1,54 @@
+﻿using PROJECT_FINAL_PRN221_GROUP3_SE1610.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PROJECT_FINAL_PRN221_GROUP3_SE1610
+{
+    /// <summary>
+    /// Interaction logic for ManageCategory.xaml
+    /// </summary>
+    public partial class ManageCategory : Window
+    {
+        shoppingMilkPrn221Context context = new shoppingMilkPrn221Context();
+        public ManageCategory()
+        {
+            InitializeComponent();
+            loadData();
+        }
+        private void loadData()
+        {
+            lvCate.ItemsSource = context.Categories.ToList();
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
