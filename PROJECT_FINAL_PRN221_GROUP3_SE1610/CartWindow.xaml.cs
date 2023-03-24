04a436c48d408aefa193d6ec8c3a1e48ed65fe72
Cuongnvhs150257 <97275308+Cuongnvhs150257@users.noreply.github.com>
2023-03-24 15:45:43 +0700

    add checkout window

--- PROJECT_FINAL_PRN221_GROUP3_SE1610/CartWindow.xaml.cs
+++ PROJECT_FINAL_PRN221_GROUP3_SE1610/CartWindow.xaml.cs
@@ -32,18 +32,25 @@ namespace PROJECT_FINAL_PRN221_GROUP3_SE1610
 
             ShoppingCart cart = ShoppingCart.GetCart();
             lvCart.ItemsSource = cart.GetCartItems();
-            txtTotal.Text = cart.GetTotal().ToString(".00");
+            txtTotal.Text = cart.GetTotal().ToString(".000.000 VNĐ");
             btnCheckout.IsEnabled = !string.IsNullOrEmpty(Settings.UserName) && cart.GetTotal() > 0;
         }
 
         private void btnRemove_Click(object sender, RoutedEventArgs e)
         {
-
+            Button b = sender as Button;
+            Cart c = b.CommandParameter as Cart;
+            cartMilks.RemoveFromCart(c.RecordId);
+            lvCart.ItemsSource = cartMilks.GetCartItems();
+            btnCheckout.IsEnabled = !string.IsNullOrEmpty(Settings.UserName) && cartMilks.GetTotal() > 0;
+            txtTotal.Text = cartMilks.GetTotal().ToString(".000.000 VNĐ");
         }
 
         private void btnCheckout_Click(object sender, RoutedEventArgs e)
         {
-
+            CheckoutWindow checkoutWindow = new CheckoutWindow();
+            checkoutWindow.ShowDialog();
+            this.Close();
         }
     }
 }
---
+++ PROJECT_FINAL_PRN221_GROUP3_SE1610/CheckoutWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using PROJECT_FINAL_PRN221_GROUP3_SE1610.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PROJECT_FINAL_PRN221_GROUP3_SE1610
+{
+    /// <summary>
+    /// Interaction logic for CheckoutWindow.xaml
+    /// </summary>
+
+    public partial class CheckoutWindow : Window
+    {
+        shoppingMilkPrn221Context context;
+        public CheckoutWindow()
+        {
+            InitializeComponent();
+            context = new shoppingMilkPrn221Context();
+            User user = context.Users.Where(u => u.Username == Settings.UserName).FirstOrDefault();
+            txtUsername.Text = user.Username;
+            txtFullname.Text = user.FullName;
+            txtAddress.Text = user.Address;
+            txtPhone.Text = user.Phone;
+            txtTotal.Text = ShoppingCart.GetCart().GetTotal().ToString(".000.000 VNĐ");
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            User user = context.Users.Where(u => u.Username == Settings.UserName).FirstOrDefault();
+            txtUsername.Text = user.Username;
+            Order order = new Order
+            {
+                Username = Settings.UserName,
+                OrderDate = DateTime.Now,
+                Address = txtAddress.Text,
+                Phone = txtPhone.Text,
+                UserId = user.UserId,
+                Total = ShoppingCart.GetCart().GetTotal()
+
+            };
+            ShoppingCart cart = ShoppingCart.GetCart();
+            int orderId = cart.CreateOrder(order);
+            MessageBox.Show($"Đã đặt hàng thành công, OrderId của bạn là =  {orderId} ");
+            this.Close();
+        }
+    }
+}
