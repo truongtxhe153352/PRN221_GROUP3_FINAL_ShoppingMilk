09ee49de44222a3d9b796926d5a2ee63b730f7ef
Cuongnvhs150257 <97275308+Cuongnvhs150257@users.noreply.github.com>
2023-03-24 20:42:14 +0700

    add code to manage brand, cate, user

--- PROJECT_FINAL_PRN221_GROUP3_SE1610/ManageBrand.xaml.cs
+++ PROJECT_FINAL_PRN221_GROUP3_SE1610/ManageBrand.xaml.cs
@@ -34,22 +34,101 @@ namespace PROJECT_FINAL_PRN221_GROUP3_SE1610
 
         private void btnSearch_Click(object sender, RoutedEventArgs e)
         {
-
+            var listSearchBrand = context.Brands.Where(o => o.BrandName.Contains(txtSearch.Text)).ToList();
+            lvBrand.ItemsSource = listSearchBrand;
         }
 
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                if (!string.IsNullOrEmpty(txtBrandName.Text))
+                {
+                    var brand = new Brand();
+                    brand.BrandName = txtBrandName.Text;
+                    context.Add(brand);
+                    if (context.SaveChanges() > 0)
+                    {
+                        MessageBox.Show("add successfully");
+                        loadData();
+                    }
+                    else
+                    {
+                        MessageBox.Show("add fail");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Brand Name is empty");
+                }
+            }
+            catch(Exception ex)
+            {
 
+            }
         }
 
         private void btnEdit_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                if (!string.IsNullOrEmpty(txtBrandName.Text))
+                {
+                    var brand = lvBrand.SelectedItem as Brand;
+                    brand.BrandName = txtBrandName.Text;
+                    context.Update(brand);
+                    if (context.SaveChanges() > 0)
+                    {
+                        MessageBox.Show("Update successfully");
+                        loadData();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Update fail");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Brand Name is empty");
+                }
+            }
+            catch(Exception ex)
+            {
 
+            }
         }
 
         private void btnDelete_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                var brand = lvBrand.SelectedItem as Brand;
+                if (brand != null)
+                {
+                    MessageBoxResult messageResult = System.Windows.MessageBox.Show("Are you sure?", "Delete Confirmation", System.Windows.MessageBoxButton.YesNo);
+                    if (messageResult == MessageBoxResult.No)
+                    {
+                        return;
+                    }
+                    try
+                    {
+                        context.Brands.Remove(brand);
+                        if (context.SaveChanges() > 0)
+                        {
+                            MessageBox.Show("Delete success");
+                            loadData();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
 
+            }
         }
     }
 }
--- PROJECT_FINAL_PRN221_GROUP3_SE1610/ManagerUser.xaml.cs
+++ PROJECT_FINAL_PRN221_GROUP3_SE1610/ManagerUser.xaml.cs
@@ -33,17 +33,117 @@ namespace PROJECT_FINAL_PRN221_GROUP3_SE1610
 
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                if (!string.IsNullOrEmpty(txtUsername.Text)
+                || !string.IsNullOrEmpty(txtPassword.Text)
+                || !string.IsNullOrEmpty(txtFullname.Text)
+                || !string.IsNullOrEmpty(txtAddress.Text)
+                || !string.IsNullOrEmpty(txtEmail.Text)
+                || !string.IsNullOrEmpty(txtPhone.Text)
+                || !string.IsNullOrEmpty(txtGender.Text)
+                || dpBirthDate.SelectedDate == null)
+                {
+                    MessageBox.Show("Add faild");
+                    return;
+                }
+                User user = new User();
+                user.Username = txtUsername.Text;
+                user.Passwork = txtPassword.Text;
+                user.Address = txtAddress.Text;
+                user.Email = txtEmail.Text;
+                user.Phone = txtPhone.Text;
+                user.Gender = txtGender.Text;
+                user.BirthDate = dpBirthDate.SelectedDate;
+                context.Users.Add(user);
+                if (context.SaveChanges() > 0)
+                {
+                    MessageBox.Show("add successfully");
+                    loadData();
+                }
+                else
+                {
+                    MessageBox.Show("add fail");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("add fail");
+            }
         }
 
         private void btnEdit_Click(object sender, RoutedEventArgs e)
         {
+            try {
+                if (!string.IsNullOrEmpty(txtUsername.Text)
+                || !string.IsNullOrEmpty(txtPassword.Text)
+                || !string.IsNullOrEmpty(txtFullname.Text)
+                || !string.IsNullOrEmpty(txtAddress.Text)
+                || !string.IsNullOrEmpty(txtEmail.Text)
+                || !string.IsNullOrEmpty(txtPhone.Text)
+                || !string.IsNullOrEmpty(txtGender.Text)
+                || dpBirthDate.SelectedDate == null)
+                {
+                    MessageBox.Show("Edit faild");
+                    return;
+                }
+                var user = lvUser.SelectedItem as User;
+                user.Username = txtUsername.Text;
+                user.Passwork = txtPassword.Text;
+                user.Address = txtAddress.Text;
+                user.Email = txtEmail.Text;
+                user.Phone = txtPhone.Text;
+                user.Gender = txtGender.Text;
+                user.BirthDate = dpBirthDate.SelectedDate;
+                context.Users.Update(user);
+                if (context.SaveChanges() > 0)
+                {
+                    MessageBox.Show("Update successfully");
+                    loadData();
+                }
+                else
+                {
+                    MessageBox.Show("Update fail");
+                }
 
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Update fail");
+            }
         }
 
         private void btnDelete_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                var user = lvUser.SelectedItem as User;
+                if (user != null)
+                {
+                    MessageBoxResult messageResult = System.Windows.MessageBox.Show("Are you sure?", "Delete Confirmation", System.Windows.MessageBoxButton.YesNo);
+                    if (messageResult == MessageBoxResult.No)
+                    {
+                        return;
+                    }
+                    try
+                    {
+                        context.Users.Remove(user);
+                        if (context.SaveChanges() > 0)
+                        {
+                            MessageBox.Show("Delete success");
+                            loadData();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
 
+            }
         }
 
         private void btnRemove_Click(object sender, RoutedEventArgs e)
@@ -53,7 +153,8 @@ namespace PROJECT_FINAL_PRN221_GROUP3_SE1610
 
         private void btnSearch_Click(object sender, RoutedEventArgs e)
         {
-
+            var listSearchUser = context.Users.Where(o => o.Username.Contains(txtSearch.Text)).ToList();
+            lvUser.ItemsSource = listSearchUser;
         }
     }
 }
