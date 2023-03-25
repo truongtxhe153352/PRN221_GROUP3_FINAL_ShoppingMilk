53c63574dd8a4bfa844a295b5688dfd0b25ff56d
truongtxhe153352 <truongtxhe153352@fpt.edu.vn>
2023-03-25 10:23:18 +0700

    insert milk

--- PROJECT_FINAL_PRN221_GROUP3_SE1610/ManagerMilk.xaml.cs
+++ PROJECT_FINAL_PRN221_GROUP3_SE1610/ManagerMilk.xaml.cs
@@ -1,4 +1,6 @@
-﻿using PROJECT_FINAL_PRN221_GROUP3_SE1610.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32;
+using PROJECT_FINAL_PRN221_GROUP3_SE1610.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,12 +32,40 @@ namespace PROJECT_FINAL_PRN221_GROUP3_SE1610
 
         private void Button_Refresh(object sender, RoutedEventArgs e)
         {
-
+            txtMilkName.Clear();
+            txtUrl.Clear();
+            txtPrice.Clear();
+            txtQuantity.Clear();
+            dpkDate.SelectedDate = null;
+            txtDescription.Clear();
+            cbCategory.SelectedIndex = -1;
         }
 
         private void btnInsert_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                Milk milk = new Milk();
 
+                Category cate = context.Categories.SingleOrDefault(c => c.Name.Equals(cbCategory.Text));
+
+                MessageBox.Show(cate.CategoryId.ToString());
+                milk.CateId = cate.CategoryId;
+                milk.Name = txtMilkName.Text;
+                milk.Published = dpkDate.SelectedDate;
+                milk.Decription = txtDescription.Text;
+                milk.Price = double.Parse(txtPrice.Text);
+                milk.Quantity = long.Parse(txtQuantity.Text);
+                milk.ImageUrl = txtUrl.Text;
+                context.Milk.Add(milk);
+                context.SaveChanges();
+                LoadMilk();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Insert error: " + ex.Message);
+            }
         }
 
         private void btnUpdate_Click(object sender, RoutedEventArgs e)
@@ -45,7 +75,45 @@ namespace PROJECT_FINAL_PRN221_GROUP3_SE1610
 
         private void btnDelete_Click(object sender, RoutedEventArgs e)
         {
+            var milk = lvMilk.SelectedItem as Milk;
+            if (milk != null)
+            {
+                MessageBoxResult messageResult = System.Windows.MessageBox.Show($"Are you sure delete { milk.Name}?", "Delete Confirmation", System.Windows.MessageBoxButton.YesNo);
+                if (messageResult == MessageBoxResult.No)
+                {
+                    return;
+                }
+                try
+                {
+                    context.Milk.Remove(milk);
+                    if (context.SaveChanges() > 0)
+                    {
+                        MessageBox.Show($"{milk.Name} Delete success");
+                        LoadMilk();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Have error here: " + ex.Message);
+                }
+            }
+        }
 
+        private void LoadMilk()
+        {
+            lvMilk.ItemsSource = context.Milk.ToList();
+            cbCategory.ItemsSource = context.Categories.ToList().Select(r => r.Name).ToList();
+        }
+
+        private void btnUrl_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.png;*.jpeg)|*.png;*.jpeg|All Files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string fileName = openFileDialog.FileName;
+                txtUrl.Text = fileName;
+            }
         }
     }
 }
